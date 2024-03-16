4f59b8355629b0492a8e87d9252607228c4e6d00
devb119 <ducanh11092001@gmail.com>
2024-03-16 12:31:49 +0700

    code status at first result

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train_linear.py --dataset "beijing" --group_name ver1_beijing --num_epochs_stdgi 0 --decoder_epochs 50 --n_iterations 300 --name "gap_filling_lr_0.0005" --lr_stdgi 0.0005 --use_wandb
+CUDA_VISIBLE_DEVICES=1 python train_linear.py --dataset "beijing" --group_name ver1_beijing --num_epochs_stdgi 0 --decoder_epochs 50 --n_iterations 300 --name "gap_filling_2" --lr_stdgi 0.001 --use_wandb
